6bfd6e233a6f153d3c2e0f1bb52a65166a919af6
WilsonSong1 <G00418330@gmail.com>
2025-01-04 23:47:38 +0000

    Table for students

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ const port = 3004;
 const express = require('express');
 const app = express();
 let ejs = require('ejs')
+const mongoose = require('mongoose');
 
 app.set('view engine', 'ejs')
+
+var pmysql = require('promise-mysql')
+var pool
+
+//Connecting to pool and promise to mysql
+pmysql.createPool({
+    connectionLimit : 3,
+    host : 'localhost',
+    user : 'root',
+    password : 'root',
+    database : 'proj2024mysql'
+    })
+    .then((p) => {
+       pool = p
+    })
+    .catch((e) => {
+        console.log("pool error:" + e)
+  })
+
+//Connecting to MongoDB
+mongoose.connect('mongodb://localhost:27017/proj2024MongoDB', {
+})
 
 //Route for Home Page
 app.get('/', (req, res) => {
@@ -11,8 +34,10 @@ app.get('/', (req, res) => {
   });
 
   //Route for Students Page
-app.get('/students', (req, res) => {
-    res.render('students');
+app.get('/students', async(req, res) => {
+  const students = await pool.query("select * from student order by sid")
+
+  res.render('students', {students});
   });
   
   //Route for Grades Page
@@ -21,8 +46,12 @@ app.get('/students', (req, res) => {
   });
   
   //Route for Lecturers Page
-  app.get("/lecturers", async (req, res) => {
+  app.get("/lecturers", async(req, res) => {
         res.render("lecturers");
+});
+
+app.get("/students/update/:sid", async(req,res)=>{
+
 });
   
   //Start the server
