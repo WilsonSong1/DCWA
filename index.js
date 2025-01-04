494a6222e95f33eb4bc6026291d2c4dd7c7f09a2
WilsonSong1 <G00418330@gmail.com>
2025-01-04 23:21:49 +0000

    Added ejs pages

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const port = 3004;
 const express = require('express');
 const app = express();
+let ejs = require('ejs')
+
+app.set('view engine', 'ejs')
 
 //Route for Home Page
 app.get('/', (req, res) => {
-    res.send(`
-      <h1>G00418330</h1>
-      <ul>
-        <li><a href="/students">Students Page</a></li>
-        <li><a href="/grades">Grades Page</a></li>
-        <li><a href="/lecturers">Lecturers Page</a></li>
-      </ul>
-    `);
+    res.render('mainPage');
   });
 
   //Route for Students Page
 app.get('/students', (req, res) => {
-    res.send('<h1>Students Page</h1>');
+    res.render('students');
   });
   
   //Route for Grades Page
   app.get('/grades', (req, res) => {
-    res.send('<h1>Grades Page</h1>');
+    res.render('grades');
   });
   
   //Route for Lecturers Page
-  app.get('/lecturers', (req, res) => {
-    res.send('<h1>Lecturers Page</h1>');
-  });
+  app.get("/lecturers", async (req, res) => {
+        res.render("lecturers");
+});
   
   //Start the server
   app.listen(port, () => {
